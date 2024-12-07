aab2eafa7ca7f2f576323bd7c3800cf70914b390
grbzemir <eyetkingurbuz@trakya.edu.tr>
2024-12-07 13:58:53 +0300

    .

--- src/redux/reducers/card.jsx
+++ src/redux/reducers/card.jsx
@@ -1,14 +1,28 @@
-
-
-
 export const cardReducer = (state = { cardItems: [] }, action) => {
     switch (action.type) {
-        case 'ADD_CARD':
+        case 'ADD_CARD': {
+            const item = action.payload;
+            const existItem = state.cardItems.find((x) => x.id === item.id); // Burada item.payload yerine item.id olmalı
+            if (existItem) {
+                return {
+                    ...state,
+                    cardItems: state.cardItems.map(x => x.id === existItem.id ? item : x)
+                };
+            } else {
+                return {
+                    ...state,
+                    cardItems: [...state.cardItems, item]
+                };
+            }
+        }
+
+        case 'REMOVE_CARD':
             return {
                 ...state,
-                product: action.payload
-            }
+                cardItems: state.cardItems.filter(x => x.id !== action.payload)
+            };
+
         default:
-            return state
+            return state;
     }
-}
+};
